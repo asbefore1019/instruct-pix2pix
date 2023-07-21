830b5459ce0171281fe64dd6c77cd6f2f2b27a69
asbefore1019 <128290005+asbefore1019@users.noreply.github.com>
2023-07-21 15:51:57 +0800

    Update generate_img_dataset.py
    add comment

--- dataset_creation/generate_img_dataset.py
+++ dataset_creation/generate_img_dataset.py
@@ -15,6 +15,7 @@ from tqdm import tqdm
 
 sys.path.append("./")
 sys.path.append("./stable_diffusion")
+# 将当前目录下的./和./stable_diffusion路径添加到Python的模块搜索路径中
 
 from ldm.modules.attention import CrossAttention
 from ldm.util import instantiate_from_config
@@ -27,7 +28,7 @@ from metrics.clip_similarity import ClipSimilarity
 
 
 def append_dims(x, target_dims):
-    """Appends dimensions to the end of a tensor until it has target_dims dimensions."""
+    """Appends dimensions to the end of a tensor until it has target_dims dimensions.将维度附加到张量的末尾，直到它具有target_dims维度"""
     dims_to_append = target_dims - x.ndim
     if dims_to_append < 0:
         raise ValueError(f"input has {x.ndim} dims but target_dims is {target_dims}, which is less")
@@ -35,20 +36,22 @@ def append_dims(x, target_dims):
 
 
 def to_d(x, sigma, denoised):
-    """Converts a denoiser output to a Karras ODE derivative."""
+    """Converts a denoiser output to a Karras ODE derivative.将去噪器输出转换为Karras ODE导数"""
     return (x - denoised) / append_dims(sigma, x.ndim)
 
 
 def get_ancestral_step(sigma_from, sigma_to):
     """Calculates the noise level (sigma_down) to step down to and the amount
     of noise to add (sigma_up) when doing an ancestral sampling step."""
+    # 在执行原始采样法的步骤时，计算要降阶到的噪声电平(sigma_down)和要添加的噪声量(sigma_up)
     sigma_up = min(sigma_to, (sigma_to**2 * (sigma_from**2 - sigma_to**2) / sigma_from**2) ** 0.5)
     sigma_down = (sigma_to**2 - sigma_up**2) ** 0.5
     return sigma_down, sigma_up
 
 
 def sample_euler_ancestral(model, x, sigmas, prompt2prompt_threshold=0.0, **extra_args):
-    """Ancestral sampling with Euler method steps."""
+    """原始采样法+欧拉方法 Ancestral sampling with Euler method steps."""
+    """原始/祖先采样法——根据有向图的顺序，先对祖先节点进行采样，只有当某个节点的父节点都已经完成采样，才对该节点进行采样"""
     s_in = x.new_ones([x.shape[0]])
     for i in range(len(sigmas) - 1):
         prompt_to_prompt = prompt2prompt_threshold > i / (len(sigmas) - 2)
@@ -94,6 +97,10 @@ def load_model_from_config(config, ckpt, vae_ckpt=None, verbose=False):
     return model
 
 
+"""CFGDenoiser是一个继承自nn.Module的模型类。它接受一个模型作为参数，并将其作为内部模型存储在self.inner_model中。
+该模型的前向传播方法forward接受输入x、噪声标准差sigma、无条件特征uncond、条件特征cond和cfg_scale作为参数。
+在前向传播过程中，输入x和sigma被重复拼接为两份，uncond和cond被拼接为一份，然后将它们作为参数传递给内部模型self.inner_model进行计算。
+最后，根据cfg_scale对计算结果进行线性插值，得到最终的输出。"""
 class CFGDenoiser(nn.Module):
     def __init__(self, model):
         super().__init__()
@@ -107,6 +114,9 @@ class CFGDenoiser(nn.Module):
         return uncond + (cond - uncond) * cfg_scale
 
 
+"""to_pil函数接受一个torch.Tensor类型的图像，并将其转换为PIL.Image.Image类型。
+首先，函数将图像的数值范围从[0, 1]映射到[0, 255]，然后将通道维度调整为最后一个维度。
+最后，函数使用Image.fromarray将数组转换为PIL.Image.Image对象，并将其返回。"""
 def to_pil(image: torch.Tensor) -> Image.Image:
     image = 255.0 * rearrange(image.cpu().numpy(), "c h w -> h w c")
     image = Image.fromarray(image.astype(np.uint8))
@@ -121,6 +131,8 @@ def main():
         required=True,
         help="Path to output dataset directory.",
     )
+    # 用于解析命令行参数的。它使用argparse模块创建了一个ArgumentParser对象，并定义了一个--out_dir参数。
+    # --out_dir参数是一个必需的参数，用于指定输出数据集的目录路径。在命令行中运行脚本时，可以通过--out_dir参数来指定输出目录的路径。
     parser.add_argument(
         "--prompts_file",
         type=str,
@@ -309,6 +321,33 @@ def main():
                     fp.write(f"{json.dumps(dict(seed=seed, **result))}\n")
 
     print("Done.")
+    
+"""这段代码是一个主要的循环，用于生成样本并保存结果。下面是代码的大致流程：
+
+1. 使用`torch.no_grad()`上下文管理器禁用梯度计算。
+2. 使用`torch.autocast("cuda")`上下文管理器启用混合精度计算（使用半精度浮点数加速计算）。
+3. 使用`model.ema_scope()`上下文管理器启用指数移动平均（Exponential Moving Average，EMA）。
+4. 使用`model.get_learned_conditioning`方法获取学习到的条件信息（uncond）。
+5. 使用`model_wrap.get_sigmas`方法获取模型的sigmas。
+6. 对于每个prompt（在`tqdm(prompts, desc="Prompts")`中进行迭代）：
+   - 创建一个与prompt相关的目录。
+   - 将prompt保存为JSON文件。
+   - 使用`model.get_learned_conditioning`方法获取与prompt相关的条件信息（cond）。
+   - 创建一个空的`results`字典来保存生成的样本。
+   - 在`while`循环中，生成样本，直到`results`字典中的样本数量达到`opt.n_samples`。
+     - 生成一个随机种子。
+     - 使用随机种子生成噪声输入`x`。
+     - 创建一个`CFGDenoiser`模型实例`model_wrap_cfg`，用于对输入进行处理。
+     - 生成样本并进行后处理。
+     - 计算样本与prompt之间的相似度。
+     - 将生成的样本及其相关信息保存到`results`字典中。
+   - 使用CLIP过滤器选择与prompt相似度最高的样本。
+   - 保存选定的样本及其相关信息。
+7. 打印"Done."表示生成过程结束。
+
+这段代码使用了一些辅助函数和模块，如`tqdm`用于显示进度条，`json`用于读写JSON文件，`to_pil`用于将`torch.Tensor`类型的图像转换为`PIL.Image.Image`类型，
+`sample_euler_ancestral`用于生成样本，`clip_similarity`用于计算样本与prompt之间的相似度等。
+整体上，这段代码的作用是根据给定的prompt生成一定数量的样本，并根据CLIP过滤器选择与prompt相似度最高的样本进行保存。"""
 
 
 if __name__ == "__main__":
